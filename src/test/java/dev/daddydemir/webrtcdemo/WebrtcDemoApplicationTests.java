8202716618adeedae0c0cee3706e48db581a4bed
daddydemir <42716195+daddydemir@users.noreply.github.com>
2023-07-23 01:38:02 +0300

    first blood.

---
+++ src/test/java/dev/daddydemir/webrtcdemo/WebrtcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daddydemir.webrtcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
